73d6c97767ea0fd957bad159381b951100f725ad
ivantportfolio <ivantroybandico@gmail.com>
2026-05-30 23:25:36 +0800

    Add email sending functionality for contact form

---
+++ send_email.php
@@ -0,0 +1,40 @@
+<?php
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Collect and sanitize input data
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    // Your email address where messages will be sent
+    $to = "info@aguarantee.ph"; 
+    $email_subject = "New Contact Form Submission: " . $subject;
+
+    // Email body content
+    $email_body = "You have received a new message from your website contact form.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Phone: $phone\n";
+    $email_body .= "Subject: $subject\n\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        // Success - redirect back to contact page with success message
+        header("Location: contact.html?status=success");
+        exit;
+    } else {
+        // Error
+        header("Location: contact.html?status=error");
+        exit;
+    }
+}
+?>
